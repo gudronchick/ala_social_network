7c33e0a662cf18ebfae17fb27a738c16f2521cbf
Андрей <68080446+gudronchick@users.noreply.github.com>
2021-08-12 20:13:43 +0300

    Updates

---
+++ precache-manifest.b94d943cc40aa40124abbb69cac9b7f8.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b39907487b124123d156d47e802841c",
+    "url": "/ala_social_network/index.html"
+  },
+  {
+    "revision": "29aa4b030c4b913c756d",
+    "url": "/ala_social_network/static/css/3.607174ce.chunk.css"
+  },
+  {
+    "revision": "6e56b68b8edadde9593f",
+    "url": "/ala_social_network/static/css/4.fba99984.chunk.css"
+  },
+  {
+    "revision": "83ccd0b74437a0d0556f",
+    "url": "/ala_social_network/static/css/main.c035db79.chunk.css"
+  },
+  {
+    "revision": "0651d2f20d2b8a7e717f",
+    "url": "/ala_social_network/static/js/2.d45e7974.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ala_social_network/static/js/2.d45e7974.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29aa4b030c4b913c756d",
+    "url": "/ala_social_network/static/js/3.bf95bdea.chunk.js"
+  },
+  {
+    "revision": "6e56b68b8edadde9593f",
+    "url": "/ala_social_network/static/js/4.ce742b28.chunk.js"
+  },
+  {
+    "revision": "83ccd0b74437a0d0556f",
+    "url": "/ala_social_network/static/js/main.0f62be1d.chunk.js"
+  },
+  {
+    "revision": "3ee2231fc0c7d943367d",
+    "url": "/ala_social_network/static/js/runtime-main.2aab0eb1.js"
+  },
+  {
+    "revision": "82e7b64c125b3dc49c468eb1e9bceb7f",
+    "url": "/ala_social_network/static/media/band1.82e7b64c.jpg"
+  },
+  {
+    "revision": "2333baa5ccf50ae6807bca16e505b4b9",
+    "url": "/ala_social_network/static/media/donut.2333baa5.jpg"
+  },
+  {
+    "revision": "b79a185013308f705b81f66b14da7825",
+    "url": "/ala_social_network/static/media/girl.b79a1850.jpg"
+  },
+  {
+    "revision": "553af5a9e88c5af3d9861d85298cffbc",
+    "url": "/ala_social_network/static/media/main_bg.553af5a9.jpg"
+  },
+  {
+    "revision": "5d0545a601c23700fa6643843cf97779",
+    "url": "/ala_social_network/static/media/profilePhoto.5d0545a6.png"
+  },
+  {
+    "revision": "edbacdd20b4d7933120db3c8de5a893b",
+    "url": "/ala_social_network/static/media/video1.edbacdd2.mp4"
+  },
+  {
+    "revision": "97bc08a7cf8f9942f00d9fb034437f7e",
+    "url": "/ala_social_network/static/media/video2.97bc08a7.mp4"
+  },
+  {
+    "revision": "faecc0eabc7b0fc9100a503244e9513a",
+    "url": "/ala_social_network/static/media/video_img1.faecc0ea.jpg"
+  },
+  {
+    "revision": "f4000d90c6b4c957ec57e910d1ab2593",
+    "url": "/ala_social_network/static/media/video_img2.f4000d90.jpg"
+  }
+]);
